ed3e0d0e6d594fecfd4b0cbf54505cac4fc4418c
Mahmoud Zahran <mi4654110@gmail.com>
2024-08-05 18:20:14 +0300

    feat : Solve Q1

--- LINQ01/Program.cs
+++ LINQ01/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace LINQ01
+﻿using static LINQ01.ListGenerator;
+namespace LINQ01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region Part 01
+
+            #region 1.Find all products that are out of stock.
+            var products = ProductsList.Where(P => P.UnitsInStock == 0);
+            foreach (var product in products)
+            {
+                Console.WriteLine(product);
+            }
+            #endregion
+
+            #endregion
         }
     }
 }
