22136fb94403a030813291dda9b9319be382e617
Mahmoud Zahran <mi4654110@gmail.com>
2024-08-05 18:25:04 +0300

    feat: Solve Q2

--- LINQ01/Program.cs
+++ LINQ01/Program.cs
@@ -8,7 +8,15 @@ namespace LINQ01
             #region Part 01
 
             #region 1.Find all products that are out of stock.
-            var products = ProductsList.Where(P => P.UnitsInStock == 0);
+            //var products = ProductsList.Where(P => P.UnitsInStock == 0);
+            //foreach (var product in products)
+            //{
+            //    Console.WriteLine(product);
+            //}
+            #endregion
+
+            #region 2.Find all products that are in stock and cost more than 3.00 per unit.
+            var products = ProductsList.Where(P => P.UnitsInStock > 0 && P.UnitPrice > 3.00M);
             foreach (var product in products)
             {
                 Console.WriteLine(product);
