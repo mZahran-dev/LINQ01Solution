27211954a9ca73dda924e79de3b4dd4c3a0dc2fa
Mahmoud Zahran <mi4654110@gmail.com>
2024-08-05 18:40:57 +0300

    feat: Solve Q3

--- LINQ01/Program.cs
+++ LINQ01/Program.cs
@@ -16,10 +16,19 @@ namespace LINQ01
             #endregion
 
             #region 2.Find all products that are in stock and cost more than 3.00 per unit.
-            var products = ProductsList.Where(P => P.UnitsInStock > 0 && P.UnitPrice > 3.00M);
-            foreach (var product in products)
+            //var products = ProductsList.Where(P => P.UnitsInStock > 0 && P.UnitPrice > 3.00M);
+            //foreach (var product in products)
+            //{
+            //    Console.WriteLine(product);
+            //}
+            #endregion
+
+            #region 3.Returns digits whose name is shorter than their value.
+            string[] arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            var shorterNameDigits = arr.Where((name,index) => name.Length < index).ToList();
+            foreach (var digit in shorterNameDigits)
             {
-                Console.WriteLine(product);
+                Console.WriteLine(digit);
             }
             #endregion
 
