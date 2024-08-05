600996f937fbdbaa32a9f2dcf7084b4705c5ca88
Mahmoud Zahran <mi4654110@gmail.com>
2024-08-05 19:09:47 +0300

    fix : modify a logic in ThenBy method

--- LINQ01/Program.cs
+++ LINQ01/Program.cs
@@ -65,7 +65,7 @@ namespace LINQ01
 
             #region 4.Sort a list of digits, first by length of their name, and then alphabetically by the name itself.
             string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-            var SortedList = Arr.OrderBy(x => x.Length).ThenBy(N => N.Length);
+            var SortedList = Arr.OrderBy(x => x.Length).ThenBy(N => N);
             foreach (var sortedList in SortedList)
             {
                 Console.WriteLine(sortedList);
